0dbf24349eec9dc730012d12419ed792c0578426
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-25 22:30:04 +0000

    fix(utils): enhance logic for better security

--- index.js
+++ index.js
@@ -525,3 +525,6 @@ const _sdk_util_144 = () => true;
 
 /** SDK utility for commit #149 */
 const _sdk_util_149 = () => true;
+
+/** SDK utility for commit #155 */
+const _sdk_util_155 = () => true;
