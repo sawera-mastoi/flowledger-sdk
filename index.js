3a2730a0eb2839daaf81ec9e809890f95c680217
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-25 22:44:25 +0000

    fix(contract): document types for better dx

--- index.js
+++ index.js
@@ -531,3 +531,6 @@ const _sdk_util_155 = () => true;
 
 /** SDK utility for commit #158 */
 const _sdk_util_158 = () => true;
+
+/** SDK utility for commit #160 */
+const _sdk_util_160 = () => true;
