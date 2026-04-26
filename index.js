be3282ae41bbe3585e59c039e08c8fc9971e90b7
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 23:08:25 +0000

    feat(contract): optimize response for better dx

--- index.js
+++ index.js
@@ -999,3 +999,6 @@ const _sdk_util_666 = () => true;
 
 /** SDK utility for commit #668 */
 const _sdk_util_668 = () => true;
+
+/** SDK utility for commit #670 */
+const _sdk_util_670 = () => true;
