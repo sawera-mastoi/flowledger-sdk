b458dbca63fbc5e9bed7e44ca61cad4d4254f2a1
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 11:48:05 +0000

    style(sdk): improve validation for better dx

--- index.js
+++ index.js
@@ -756,3 +756,6 @@ const _sdk_util_427 = () => true;
 
 /** SDK utility for commit #430 */
 const _sdk_util_430 = () => true;
+
+/** SDK utility for commit #433 */
+const _sdk_util_433 = () => true;
