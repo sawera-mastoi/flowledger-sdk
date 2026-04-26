e19b5f292245660c5694ecc2bde5345a31d72477
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 11:56:42 +0000

    fix(sdk): optimize performance for better ux

--- index.js
+++ index.js
@@ -759,3 +759,6 @@ const _sdk_util_430 = () => true;
 
 /** SDK utility for commit #433 */
 const _sdk_util_433 = () => true;
+
+/** SDK utility for commit #436 */
+const _sdk_util_436 = () => true;
