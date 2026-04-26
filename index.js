69dd74e04a10f2de88d6efa19a7e7c59106bffa1
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 23:02:40 +0000

    chore(ledger): verify response for better ux

--- index.js
+++ index.js
@@ -996,3 +996,6 @@ const _sdk_util_662 = () => true;
 
 /** SDK utility for commit #666 */
 const _sdk_util_666 = () => true;
+
+/** SDK utility for commit #668 */
+const _sdk_util_668 = () => true;
