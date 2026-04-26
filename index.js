4fbb486dc51a442a4c5bffa98ed6ba5ba8ac9f66
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 11:59:34 +0000

    refactor(sdk): improve headers for better reliability

--- index.js
+++ index.js
@@ -762,3 +762,6 @@ const _sdk_util_433 = () => true;
 
 /** SDK utility for commit #436 */
 const _sdk_util_436 = () => true;
+
+/** SDK utility for commit #437 */
+const _sdk_util_437 = () => true;
