efa118c46bb352c7e1ea2d0988f5ae2630e22f97
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 09:33:10 +0000

    feat(contract): fix headers for better dx

--- index.js
+++ index.js
@@ -711,3 +711,6 @@ const _sdk_util_377 = () => true;
 
 /** SDK utility for commit #378 */
 const _sdk_util_378 = () => true;
+
+/** SDK utility for commit #386 */
+const _sdk_util_386 = () => true;
