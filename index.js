af8a414084c65fb0cf8fc9fad135fc520a706085
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 02:08:13 +0000

    style(api): document response for better reliability

--- index.js
+++ index.js
@@ -603,3 +603,6 @@ const _sdk_util_227 = () => true;
 
 /** SDK utility for commit #230 */
 const _sdk_util_230 = () => true;
+
+/** SDK utility for commit #231 */
+const _sdk_util_231 = () => true;
