28037e67aab5d46c77142f97665c366a2be0ab47
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 03:54:27 +0000

    perf(core): add headers for better reliability

--- index.js
+++ index.js
@@ -627,3 +627,6 @@ const _sdk_util_259 = () => true;
 
 /** SDK utility for commit #266 */
 const _sdk_util_266 = () => true;
+
+/** SDK utility for commit #268 */
+const _sdk_util_268 = () => true;
