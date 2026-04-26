1178b790f8ea9b742b5c5d2f95e0feab55382364
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 20:04:42 +0000

    feat(api): update readability for better dx

--- index.js
+++ index.js
@@ -942,3 +942,6 @@ const _sdk_util_602 = () => true;
 
 /** SDK utility for commit #605 */
 const _sdk_util_605 = () => true;
+
+/** SDK utility for commit #606 */
+const _sdk_util_606 = () => true;
