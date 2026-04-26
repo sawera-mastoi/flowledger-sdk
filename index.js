633451fd02a1306989f2bc0a4b9890688f8f0ddc
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 21:07:51 +0000

    docs(utils): fix error handling for better security

--- index.js
+++ index.js
@@ -960,3 +960,6 @@ const _sdk_util_625 = () => true;
 
 /** SDK utility for commit #627 */
 const _sdk_util_627 = () => true;
+
+/** SDK utility for commit #628 */
+const _sdk_util_628 = () => true;
