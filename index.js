d9b77a26be840991b7d153916d9d2a5c24dc5c3a
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 14:08:45 +0000

    chore(api): optimize readability for better dx

--- index.js
+++ index.js
@@ -804,3 +804,6 @@ const _sdk_util_474 = () => true;
 
 /** SDK utility for commit #478 */
 const _sdk_util_478 = () => true;
+
+/** SDK utility for commit #482 */
+const _sdk_util_482 = () => true;
