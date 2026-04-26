7c84b06661037487306026dc672150221d8ecd0b
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 19:53:13 +0000

    chore(ledger): add constants for better reliability

--- index.js
+++ index.js
@@ -936,3 +936,6 @@ const _sdk_util_599 = () => true;
 
 /** SDK utility for commit #600 */
 const _sdk_util_600 = () => true;
+
+/** SDK utility for commit #602 */
+const _sdk_util_602 = () => true;
