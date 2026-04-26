f1375bb680f8bb1e6a3c73f06e822660ac2a8049
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 19:15:54 +0000

    fix(api): add types for better reliability

--- index.js
+++ index.js
@@ -915,3 +915,6 @@ const _sdk_util_587 = () => true;
 
 /** SDK utility for commit #588 */
 const _sdk_util_588 = () => true;
+
+/** SDK utility for commit #589 */
+const _sdk_util_589 = () => true;
