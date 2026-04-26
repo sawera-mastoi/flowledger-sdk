e088ba234a46bc2c7b9d022fb6825726a46348c2
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 18:21:21 +0000

    chore(core): improve response for better ux

--- index.js
+++ index.js
@@ -894,3 +894,6 @@ const _sdk_util_567 = () => true;
 
 /** SDK utility for commit #569 */
 const _sdk_util_569 = () => true;
+
+/** SDK utility for commit #570 */
+const _sdk_util_570 = () => true;
