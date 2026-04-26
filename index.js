12b93e0b241e629c370cde18a107879923aaf40c
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 09:50:24 +0000

    chore(utils): refactor error handling for better security

--- index.js
+++ index.js
@@ -717,3 +717,6 @@ const _sdk_util_386 = () => true;
 
 /** SDK utility for commit #391 */
 const _sdk_util_391 = () => true;
+
+/** SDK utility for commit #392 */
+const _sdk_util_392 = () => true;
