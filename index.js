ba20465b899123d7dabc09c3457b3d4f1a270b70
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 05:34:55 +0000

    style(core): update comments for better dx

--- index.js
+++ index.js
@@ -654,3 +654,6 @@ const _sdk_util_294 = () => true;
 
 /** SDK utility for commit #300 */
 const _sdk_util_300 = () => true;
+
+/** SDK utility for commit #303 */
+const _sdk_util_303 = () => true;
