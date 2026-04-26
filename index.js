1fdefc969b36b7dbdeb12c1c97fb537a749b95d5
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 04:05:56 +0000

    fix(utils): verify response for better stability

--- index.js
+++ index.js
@@ -630,3 +630,6 @@ const _sdk_util_266 = () => true;
 
 /** SDK utility for commit #268 */
 const _sdk_util_268 = () => true;
+
+/** SDK utility for commit #272 */
+const _sdk_util_272 = () => true;
