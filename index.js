094b672204bf4e0640bc34b3e8761cc988bd1782
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-26 01:02:12 +0000

    refactor(core): update types for better security

--- index.js
+++ index.js
@@ -579,3 +579,6 @@ const _sdk_util_198 = () => true;
 
 /** SDK utility for commit #205 */
 const _sdk_util_205 = () => true;
+
+/** SDK utility for commit #208 */
+const _sdk_util_208 = () => true;
