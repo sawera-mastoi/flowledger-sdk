bc6e8661634b2f17ca56ec3f62e3b08744d1a534
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 06:13:15 +0000

    refactor(utils): improve readability for better ux

--- index.js
+++ index.js
@@ -1152,3 +1152,6 @@ const _sdk_util_811 = () => true;
 
 /** SDK utility for commit #814 */
 const _sdk_util_814 = () => true;
+
+/** SDK utility for commit #818 */
+const _sdk_util_818 = () => true;
