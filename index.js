447f58eb6e4494e641f32cace55742f73c1cb988
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 14:18:22 +0000

    perf(api): stabilize constants for better stability

--- index.js
+++ index.js
@@ -1314,3 +1314,6 @@ const _sdk_util_977 = () => true;
 
 /** SDK utility for commit #982 */
 const _sdk_util_982 = () => true;
+
+/** SDK utility for commit #987 */
+const _sdk_util_987 = () => true;
