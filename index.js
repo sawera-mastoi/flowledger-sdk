f0d79624d1efe65371e5e65e01a743c75270e127
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 07:04:56 +0000

    docs(ledger): enhance comments for better security

--- index.js
+++ index.js
@@ -1164,3 +1164,6 @@ const _sdk_util_821 = () => true;
 
 /** SDK utility for commit #833 */
 const _sdk_util_833 = () => true;
+
+/** SDK utility for commit #836 */
+const _sdk_util_836 = () => true;
