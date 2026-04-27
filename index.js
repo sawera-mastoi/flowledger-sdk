e42d969a1698e5fc55a6b896a5f97cf6f939ddf5
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 01:52:02 +0000

    feat(api): update validation for better stability

--- index.js
+++ index.js
@@ -1059,3 +1059,6 @@ const _sdk_util_723 = () => true;
 
 /** SDK utility for commit #726 */
 const _sdk_util_726 = () => true;
+
+/** SDK utility for commit #727 */
+const _sdk_util_727 = () => true;
