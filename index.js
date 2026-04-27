5536ea3c7a7e42241c49fa0683e2e954dbff1b91
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 07:13:32 +0000

    fix(utils): enhance readability for better ux

--- index.js
+++ index.js
@@ -1170,3 +1170,6 @@ const _sdk_util_836 = () => true;
 
 /** SDK utility for commit #838 */
 const _sdk_util_838 = () => true;
+
+/** SDK utility for commit #839 */
+const _sdk_util_839 = () => true;
