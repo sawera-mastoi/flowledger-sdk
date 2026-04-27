ba1ef14571575b9a2028bfcdfa1a7dbd3a1a11bc
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 04:01:13 +0000

    perf(utils): improve error handling for better ux

--- index.js
+++ index.js
@@ -1101,3 +1101,6 @@ const _sdk_util_767 = () => true;
 
 /** SDK utility for commit #770 */
 const _sdk_util_770 = () => true;
+
+/** SDK utility for commit #772 */
+const _sdk_util_772 = () => true;
