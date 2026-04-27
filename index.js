47d59f6a31fc63a4c2bfb4b4e310b056deeab23a
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 06:01:46 +0000

    chore(ledger): improve response for better reliability

--- index.js
+++ index.js
@@ -1149,3 +1149,6 @@ const _sdk_util_810 = () => true;
 
 /** SDK utility for commit #811 */
 const _sdk_util_811 = () => true;
+
+/** SDK utility for commit #814 */
+const _sdk_util_814 = () => true;
