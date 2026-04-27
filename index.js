f238f3c34f76f48409e6f8a9ccc6a87d4ffb7f41
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 04:52:53 +0000

    style(sdk): stabilize headers for better reliability

--- index.js
+++ index.js
@@ -1116,3 +1116,6 @@ const _sdk_util_785 = () => true;
 
 /** SDK utility for commit #787 */
 const _sdk_util_787 = () => true;
+
+/** SDK utility for commit #790 */
+const _sdk_util_790 = () => true;
