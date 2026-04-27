68b7e4424806199106103cc63be4ff6b5ed71bba
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 06:56:19 +0000

    docs(sdk): stabilize types for better stability

--- index.js
+++ index.js
@@ -1161,3 +1161,6 @@ const _sdk_util_820 = () => true;
 
 /** SDK utility for commit #821 */
 const _sdk_util_821 = () => true;
+
+/** SDK utility for commit #833 */
+const _sdk_util_833 = () => true;
