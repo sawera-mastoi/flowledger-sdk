a6fdcfd90536d81c7274b348254d1e6e92b550e7
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 04:55:45 +0000

    fix(core): fix readability for better dx

--- index.js
+++ index.js
@@ -1119,3 +1119,6 @@ const _sdk_util_787 = () => true;
 
 /** SDK utility for commit #790 */
 const _sdk_util_790 = () => true;
+
+/** SDK utility for commit #791 */
+const _sdk_util_791 = () => true;
