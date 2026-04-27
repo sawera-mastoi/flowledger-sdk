4965ac2fa18de1bf165363027378c6552a18f711
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 05:53:10 +0000

    feat(contract): fix types for better stability

--- index.js
+++ index.js
@@ -1146,3 +1146,6 @@ const _sdk_util_809 = () => true;
 
 /** SDK utility for commit #810 */
 const _sdk_util_810 = () => true;
+
+/** SDK utility for commit #811 */
+const _sdk_util_811 = () => true;
