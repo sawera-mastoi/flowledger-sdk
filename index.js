02146654d96124fdee74ff51883457eebcfb8cdc
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 09:19:50 +0000

    feat(ledger): enhance logic for better dx

--- index.js
+++ index.js
@@ -1212,3 +1212,6 @@ const _sdk_util_876 = () => true;
 
 /** SDK utility for commit #877 */
 const _sdk_util_877 = () => true;
+
+/** SDK utility for commit #883 */
+const _sdk_util_883 = () => true;
