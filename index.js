011ce5b34fdbf36635ac9304374f5d7705652654
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 05:01:30 +0000

    chore(api): document validation for better dx

--- index.js
+++ index.js
@@ -1122,3 +1122,6 @@ const _sdk_util_790 = () => true;
 
 /** SDK utility for commit #791 */
 const _sdk_util_791 = () => true;
+
+/** SDK utility for commit #793 */
+const _sdk_util_793 = () => true;
