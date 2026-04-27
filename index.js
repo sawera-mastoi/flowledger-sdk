a9b78336c62243307cfb2c6d5082c8abcb583fa8
sawera mastoi <asadkhan123456hd@gmail.com>
2026-04-27 07:59:28 +0000

    feat(stacks): stabilize response for better ux

--- index.js
+++ index.js
@@ -1182,3 +1182,6 @@ const _sdk_util_853 = () => true;
 
 /** SDK utility for commit #854 */
 const _sdk_util_854 = () => true;
+
+/** SDK utility for commit #855 */
+const _sdk_util_855 = () => true;
